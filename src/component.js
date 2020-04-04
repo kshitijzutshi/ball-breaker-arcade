bc98e80f5549cc58e51946a6bbbe1ab8b3583d3a
kshitij zutshi <kshitij.zutshi@gmail.com>
2020-04-04 13:06:14 +0530

    Update component.js

--- src/component.js
+++ src/component.js
@@ -6,4 +6,41 @@ const BG_IMG = new Image();
 
 BG_IMG.src = "/assets/images/bg.jpg";
 
-export { BG_IMG };
+const LEVEL_IMG = new Image();
+
+LEVEL_IMG.src = "/assets/images/level.png";
+
+const LIFE_IMG = new Image();
+
+LIFE_IMG.src = "/assets/images/life.png";
+
+const SCORE_IMG = new Image();
+
+SCORE_IMG.src = "/assets/images/score.png";
+
+const WALL_HIT = new Audio();
+WALL_HIT.src = "/assets/sounds/wall.mp3";
+
+const LIFE_LOST = new Audio();
+LIFE_LOST.src = "/assets/sounds/life_lost.mp3";
+
+const PADDLE_HIT = new Audio();
+PADDLE_HIT.src = "/assets/sounds/paddle_hit.mp3";
+
+const WIN = new Audio();
+WIN.src = "/assets/sounds/win.mp3";
+
+const BRICK_HIT = new Audio();
+BRICK_HIT.src = "/assets/sounds/brick_hit.mp3";
+
+export {
+  BG_IMG,
+  LEVEL_IMG,
+  LIFE_IMG,
+  SCORE_IMG,
+  WALL_HIT,
+  LIFE_LOST,
+  PADDLE_HIT,
+  WIN,
+  BRICK_HIT
+};
